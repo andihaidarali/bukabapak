3c9f608590dc6c10380ef8df2a7b22cb9342ba00
hali0990 <andihaidarali@gmail.com>
2020-04-15 03:15:00 +0800

    [MASTER] - Clean The Code With and Give some Error and Validation Handling From The Request

---
+++ bklpk.py
@@ -0,0 +1,61 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+UA = ('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) '
+      'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 '
+      'Safari/537.36')
+head = {'User-Agent': UA}
+
+class get_token():
+    """
+    Melakukan Request Ke Halaman Home Bukalapak
+    Untuk Mendapatkan Token.
+    """
+    def __init__(self):
+        try:
+            root = requests.get('https://www.bukalapak.com', headers=head)
+            get_smtg = BeautifulSoup(root.text, 'html.parser')
+            get_token = get_smtg.find('meta', attrs={'name': 'oauth-access-token'})
+            self.token = get_token['content']
+        except:
+            print("Token Tidak Ditemukan")
+
+    def __str__(self):
+        return self.token
+
+class get_id():
+    """
+    Melakukan Request Ke Halaman Mobile Toko Untuk Mendapatkan ID Toko
+    note : request langsung ke halaman root web toko akan dialihkan ke 403
+    """
+    def __init__(self, nama_toko):
+        self.nama_toko = nama_toko
+
+        try:
+            profile = ('https://m.bukalapak.com/u/{}?keywords=&price_range='
+                       '&sort=bestselling&deal=&condition=&rating=&installment='
+                       '&free_shipping_provinces=&wholesale=&page=1'.format(self.nama_toko))
+            params_prof = {'page': 1, 'sort': 'bestselling'}
+            get_profile = requests.get(url=profile, headers=head, params=params_prof)
+            parsing_profile = BeautifulSoup(get_profile.text, 'html.parser')
+            json_data = json.loads(parsing_profile.find_all('merchant-page-app')[0]['seller-data'])
+            self.id = json_data['seller']['id']
+
+        except:
+            self.id = 0
+
+    def get_total(self):
+        id_store = self.id
+        token = get_token()
+        try:
+            src_total = requests.get('https://api.bukalapak.com/stores/{}/products?'
+                                     'offset=0&limit=16&sort=bestselling&access_token={}'.format(id_store, token))
+            json_src = src_total.json()
+            data_src = json_src['meta']
+            self.total = data_src['total']
+
+            return self.total
+        except:
+            self.total = 0
+            return self.total
--- run.py
+++ run.py
@@ -1,10 +1,8 @@
-import json
-
 import requests
 from bs4 import BeautifulSoup
-
 from flask import Flask, render_template, flash, request
 from wtforms import Form, validators, StringField
+import bklpk
 
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -18,74 +16,48 @@ class InputToko(Form):
     def index():
         global name
         title = "Bukalapak Scraper"
+        inputnya = "Input Nama Toko dalam url bukapak.com/u/<strong>{nama_toko}</strong> | paste hanya nama toko"
         export = []
         form = InputToko(request.form)
         print(form.errors)
-
+        token = bklpk.get_token()
         if request.method == 'POST':
-            name = request.form['name']
-            ua = ("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) "
-                  "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 "
-                  "Safari/537.36")
-            head = {'User-Agent': ua}
-
-            # get tokens from homepage
-            root = requests.get('https://www.bukalapak.com', headers=head)
-            get_smtg = BeautifulSoup(root.text, 'html.parser')
-            get_token = get_smtg.find('meta', attrs={'name': 'oauth-access-token'})
-            token = get_token['content']
-
-            # get id toko from mobile profile pages
-            nama_toko = name
-            profile = ('https://m.bukalapak.com/u/{}?keywords=&price_range='
-                       '&sort=bestselling&deal=&condition=&rating=&installment='
-                       '&free_shipping_provinces=&wholesale=&page=1'.format(nama_toko))
-            params_prof = {'page': 1, 'sort': 'bestselling'}
-            get_profile = requests.get(url=profile, headers=head, params=params_prof)
-            parsing_profile = BeautifulSoup(get_profile.text, 'html.parser')
-            json_data = json.loads(parsing_profile.find_all('merchant-page-app')[0]['seller-data'])
-            toko_id = json_data['seller']['id']
-            id_store = toko_id
-
-            # get total barang
-            src_total = requests.get('https://api.bukalapak.com/stores/{}/products?'
-                                     'offset=0&limit=16&sort=bestselling&access_token={}'.format(id_store, token))
-            json_src = src_total.json()
-            data_src = json_src['meta']
-            total = data_src['total']
-
-            limit = int(16)
-            offset = limit - 16
-            page = 0
-
-            y = total / limit
-            if type(y) is float:
-                page = int(y) + 1
-
-            pages = 0
-
-            while pages < page:
-                src = requests.get('https://api.bukalapak.com/stores/{}/products?'
-                                   'offset={}&limit={}&sort=bestselling'
-                                   '&access_token={}'.format(id_store, offset, limit, token))
-                json_data = src.json()
-                data = json_data['data']
-                for d in data:
-                    export.append(
-                        dict(name=d["name"], harga=d["price"], url=d["url"], gambar=d["images"]["large_urls"][0],
-                             terjual=d["stats"]["sold_count"],rating=d['rating']['average_rate'],
-                             desk=BeautifulSoup(d["description"], 'html.parser').text))
-
-                pages += 1
-                offset += limit
-
-        if form.validate():
-            flash('Processing data from ' + name)
+            if form.validate():
+                name = request.form['name']
+                id_store = bklpk.get_id(name).id
+                if id_store != 0:
+                    total = 16 # total = bklpk.get_id(name).get_total()
+                    limit = int(16)
+                    offset = limit - 16
+                    y = total / limit
+                    page = 0
+                    if type(y) is float:
+                        page = int(y) + 1
+                    pages = 0
+                    while pages < page:
+                        src = requests.get('https://api.bukalapak.com/stores/{}/products?'
+                                           'offset={}&limit={}&sort=bestselling'
+                                           '&access_token={}'.format(id_store, offset, limit, token))
+                        json_data = src.json()
+                        data = json_data['data']
+                        for d in data:
+                            # TODO : Buat If Else jika Avereage Rate Not Found
+                            export.append(
+                                dict(name=d["name"], harga=d["price"], url=d["url"],
+                                     gambar=d["images"]["large_urls"][0], terjual=d["stats"]["sold_count"],
+                                     rating=d['rating']['average_rate'],
+                                     desk=BeautifulSoup(d["description"], 'html.parser').text)
+                            )
+                        pages += 1
+                        offset += limit
+                else:
+                    flash("ID Toko Tidak Ditemukan")
         else:
-            flash('Input Nama Toko dalam url bukapak.com/u/<strong>{nama_toko}</strong> | paste hanya nama toko')
-
+            flash(inputnya)
         return render_template('index.html', title=title, form=form, data=export)
 
+        # TODO : Buat Halaman Lain Untuk Scarping Official Page
+        # TODO : Buat Halaman Lain Untuk Scraping Hasil Pencarian
 
 if __name__ == '__main__':
     app.run(debug=True)
